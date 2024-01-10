008c136107a40700447063c7db26cdb1f60f4018
Abdul Wahab Amir <83786802+A-Wahab-Aamir@users.noreply.github.com>
2024-01-09 20:41:54 -1100

    dictionary api

---
+++ project_6/file.js
@@ -0,0 +1,19 @@
+var url="https://api.dictionaryapi.dev/api/v2/entries/en/"
+
+
+let dict = () => {
+    var inpword=document.getElementById('inpword').value;
+
+    fetch(`${url}${inpword}`)
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+        document.getElementById('word').innerHTML = data[0].word
+        document.getElementById('def').innerHTML = data[0].meanings[0].definitions[0].definition;
+        document.getElementById('pro').innerHTML = data[0].phonetic
+        document.getElementById('part').innerHTML= data[0].meanings[0].partOfSpeech
+    })
+}
+
+
+dict()
